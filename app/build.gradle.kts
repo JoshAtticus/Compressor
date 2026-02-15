a05124b2ad9ec8d8ddfe88add95b8e24376c71c1
JoshAtticus <git@joshattic.us>
2026-02-15 18:11:39 +0800

    fine

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         applicationId = "compress.joshattic.us"
         minSdk = 24
         targetSdk = 36
-        versionCode = 202602151809
+        versionCode = 17
         versionName = "1.5.1 (Experimental, google-pixel-10-fix branch)"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
