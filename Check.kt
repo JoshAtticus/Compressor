2b61e74665791aaf303820d19887a8963af3b6d9
Josh <git@joshattic.us>
2026-03-20 17:39:48 +0800

    oops left a testing file in there

--- Check.kt
+++
@@ -1,7 +0,0 @@
-import androidx.media3.transformer.VideoEncoderSettings
-
-fun main() {
-    val b = VideoEncoderSettings.Builder()
-    b.setBitrate(100)
-    // b.setBitrateMode(VideoEncoderSettings.BITRATE_MODE_CQ)
-}
